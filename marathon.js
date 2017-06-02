12d7b73fe91c27efea4b90860528e8fee6cff7cf
dfenjves <fenjves.daniel@gmail.com>
2017-06-02 19:31:59 -0400

    modified tests

--- marathon.js
+++ marathon.js
@@ -3,6 +3,7 @@
 function timeToDecimal(time){
   var timeArray = time.split(":");
   var decimalTime = parseInt(timeArray[0]) + (parseInt(timeArray[1])/60) + (parseInt(timeArray[2])/3600);
+  decimalTime = Math.round(decimalTime*10000)/10000
   return decimalTime;
   //Need to round decimalTime to 4 decimal places
 }
@@ -12,8 +13,9 @@ function decimalToTime(decimalTime){
   newTime[0] = Math.floor(decimalTime)
   newTime[1] = Math.floor((decimalTime * 60) % 60)
   newTime[2] = Math.round(decimalTime*3600 - (newTime[0]*3600 + newTime[1]*60))
+  var stringTime = [String(newTime[0]).padStart(2).replace(" ", "0"), String(newTime[1]).padStart(2).replace(" ", "0"), String(newTime[2]).padStart(2).replace(" ", "0")]
   //need to format single digit numbers with two digits ie "01"
-  return newTime.join(":")
+  return stringTime.join(":")
 
 }
 
@@ -28,5 +30,5 @@ function marathonToPacePerMile(marathonTime){
   var decimalMarathonTime = timeToDecimal(marathonTime)
   var decimalPaceTime = decimalMarathonTime/26.2
   var finalPaceTime = decimalToTime(decimalPaceTime)
-  return "Pace time " + finalPaceTime
+  return "Pace time " + finalPaceTime.replace("00:","")
 }
